91c717815ef123ebc64c99d56aa402904de9fd9a
Benjamin <beauvais.benjamin@hotmail.fr>
2016-10-22 15:21:21 +0200

    Update and rename estat.js to exoplayer.js

---
+++ www/exoplayer.js
@@ -0,0 +1,31 @@
+//
+//  exoplayer.js
+//
+// estat Cordova Plugin by Benjamin BEAUVAIS on 22/10/16.
+//
+// exoplayer Plugin for Cordova Android
+// www.lanewscompany.com
+//
+// MIT Licensed
+
+
+function exoplayer(){}
+
+// Call this to register for push notifications and retreive a push Token
+exoplayer.initPlayer = function(serial,success, fail) {
+	cordova.exec(success, fail, "exoplayer", "initPlayer");
+};
+
+exoplayer.play = function(levels,success, fail) {
+	cordova.exec(success, fail, "exoplayer", "play", [url]);
+};
+
+// Event spawned when a notification is received while the application is active
+exoplayer.notificationCallback = function(notification) {
+	var ev = document.createEvent('HTMLEvents');
+	ev.notification = notification;
+	ev.initEvent('push-notification', true, true, arguments);
+	document.dispatchEvent(ev);
+};
+
+module.exports = exoplayer;
